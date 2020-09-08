b129649a384878ac4bd8197a4603519600dce843
Gabriel Gibson <69363005+gibsongabriel21@users.noreply.github.com>
2020-09-08 13:03:09 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,57 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1 id="history-lesson">History of code</h1>
+<hr>
+<div class="colossus">
+<h2><strong>1944 - The Colossus</strong></h2>
+<p><strong>The Colossus</strong> was the first large-scale electronic computer.
+ The inventor of Colossus was <em>Tommy Flowers</em>. He had been tasked with designing
+ a machine that would help decode “enigma”, the german code that was used during
+ World War 2.  Although current devices were able to decipher the enigma,
+ it was at a very slow rate that could take weeks at a time.
+ The Colossus accomplished this task via valves that would allow it to go
+ through codes in a much shorter amount of time than any human.
+ The idea was met with skepticism, but was eventually approved and
+ the machine was able to go through 5000 characters in a single second.
+ The Colossus was able to bring decoding that would ordinarily take weeks down
+ to only a few hours. The Colossus proved itself instrumental in ending
+ World War II, as it allowed the allied powers to learn about strategies,
+ numbers, and various other information which allowed them to overwhelm the
+ German armies. The Colossus was one of the first electric computers that would
+ eventually lead to further innovations in coding."</p>
+
+<a href="https://www.britannica.com/technology/Colossus-computer">Source</a>
+</div>
+<hr>
+
+<div class="baby">
+<h2>1948 - Manchester “Baby”</h2>
+<p>After the colossus, further advancements were made in computer science.
+ <em>Tom Kilburn</em> and <em>Frederic Williams</em> created one of  the first storage systems,
+ known as a “Williams Tube” which stored Cathode rays. After constructing
+ a small computer known as the <strong>Small Scale Experimental Machine</strong>, or <strong>“Baby”</strong>,
+ it was able to run this program and operated as the first working stored
+ program computer. Magnetic drums were also used as an outside holder of memory,
+ which can be seen as an inspiration for other external hard drives such as USB
+ ports and similar storage devices which wouldn’t exist if it weren’t for this
+ invention. It was the first program to actually be run on a computer.</p>
+
+<a href="https://www.britannica.com/biography/Tom-Kilburn#ref829450">Source</a>
+</div>
+
+<hr>
+<div class="XEROX">
+<h2>1974 - Xerox PARC Alto</h2>
+<p>The <strong>Xerox Alto</strong> marked a significant change in how people were able
+ to interact with computers. Up until this point, computers had to be communicated
+ with via text, as graphics were too complicated. The xerox changed this with
+ the mouse cursor, which made it significantly easier to run and interact with
+ programs on the computer. The mouse was much easier to use than the text-based
+ systems of the past, and allowed windows and icons to be moved easily.
+ This early computer went on to influence the Macintosh and other PCs which
+ went on to become much larger products.</p>
+
+<a href="https://www.computerhistory.org/revolution/input-output/14/347">Source</a>
+</div>
+`;
